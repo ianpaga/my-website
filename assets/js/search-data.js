c7d6cba3cc21e62260b44388c915b1d04aeb9786
ianpaga <57350668+ianpaga@users.noreply.github.com>
2026-03-08 22:04:23 +0000

    Deploying to gh-pages from @ ianpaga/my-website@3b7a1feb427c621e4ee037c3a372c060124093e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "For the full CV, please email ianpaga [at] gmail [dot] com.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/my-website/cv/";
